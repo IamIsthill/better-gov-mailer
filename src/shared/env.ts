2fe452ded328723570228e2e36fd8b6b80d2d09d
IamIsthill <bercasiocharles14@gmail.com>
2025-09-23 22:29:43 +0800

    refactor: change env

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -3,11 +3,16 @@ import dotenv from "dotenv";
 import { resolve } from "node:path";
 import process from "node:process";
 
-// Determine environment and load corresponding env file
-const env = process.env.NODE_ENV || "production";
-const envFile = env === "production" ? ".env" : ".env.dev";
-const path = resolve(process.cwd(), envFile);
-dotenv.config({ path });
+let env: object;
+
+if (Deno.env.get("NODE_ENV") == "development") {
+  const envFile = ".env.dev";
+  const path = resolve(process.cwd(), envFile);
+  dotenv.config({ path });
+  env = process.env;
+} else {
+  env = Deno.env.toObject();
+}
 
 const ENV_SCHEMA = z.object({
   PORT: z.coerce.number(),
@@ -15,7 +20,7 @@ const ENV_SCHEMA = z.object({
   WHITELIST: z.string(),
 });
 
-const { data, error } = ENV_SCHEMA.safeParse(process.env);
+const { data, error } = ENV_SCHEMA.safeParse(env);
 
 if (error) {
   console.error(error.issues);
